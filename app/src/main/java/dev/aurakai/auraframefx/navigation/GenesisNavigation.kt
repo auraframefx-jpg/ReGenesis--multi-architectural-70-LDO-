AuraFrameFxDev/ReGenesis

Implement instant color switching like Iconify
Features:
- Added setTheme() and setColor() methods to ThemeViewModel for instant updates
- Created InstantColorPickerScreen with Material You-style color chips
- Tap a color chip -> instant app-wide theme change (no delay, no confirmation)
- Works on Android 12+ without root (uses existing dynamic color system)
- Animated color chips with selection indicator
- Cyberpunk neon noir color palette (12 preset colors)
- Status bars and navigation bars update instantly via existing SideEffect

Technical:
- ThemeViewModel now has direct setters that update StateFlow immediately
- Color changes trigger recomposition of entire app theme
- Persistence happens in background (TODO: wire to DataStore)
- Wired into navigation at 'chromacore_colors' and 'instant_color_picker' routes

UI/UX:
- Clean grid layout (3 columns)
- Spring animations on selection
- Info card explaining instant switching capability
- Current color indicator at bottom

This matches Iconify's instant theme switching behavior.

app/src/main/java/dev/aurakai/auraframefx/navigation/GenesisNavigation.kt
@@ -33,6 +33,7 @@ import dev.aurakai.auraframefx.ui.customization.GyroscopeCustomizationScreen
 import dev.aurakai.auraframefx.ui.gates.AgentMonitoringScreen
 import dev.aurakai.auraframefx.ui.gates.AurasLabScreen
 import dev.aurakai.auraframefx.ui.gates.ChromaCoreColorsScreen
+import dev.aurakai.auraframefx.ui.gates.InstantColorPickerScreen
 import dev.aurakai.auraframefx.ui.gates.CodeAssistScreen
 import dev.aurakai.auraframefx.ui.gates.DirectChatScreen
 import dev.aurakai.auraframefx.ui.gates.GateNavigationScreen
@@ -252,7 +253,10 @@ fun GenesisNavigationHost(
                 UIUXGateSubmenuScreen(navController = navController)
             }
             composable("chromacore_colors") {
-                ChromaCoreColorsScreen(onNavigateBack = { navController.popBackStack() })
+                InstantColorPickerScreen(onNavigateBack = { navController.popBackStack() })
+            }
+            composable("instant_color_picker") {
+                InstantColorPickerScreen(onNavigateBack = { navController.popBackStack() })
             }
             composable(GenesisRoutes.FIREWALL) {
                 FirewallScreen()

auraframefx-jpg/ReGenesis--multi-architectural-70-LDO-

app/src/main/java/dev/aurakai/auraframefx/navigation/GenesisNavigation.kt
@@ -31,6 +31,8 @@ import dev.aurakai.auraframefx.oracledrive.genesis.cloud.OracleDriveScreen
 import dev.aurakai.auraframefx.ui.customization.GyroscopeCustomizationScreen
 import dev.aurakai.auraframefx.ui.gates.AgentHubScreen
 import dev.aurakai.auraframefx.ui.gates.AgentMonitoringScreen
+import dev.aurakai.auraframefx.ui.gates.AurasLabScreen
+import dev.aurakai.auraframefx.ui.gates.ChromaCoreColorsScreen
 import dev.aurakai.auraframefx.ui.gates.CodeAssistScreen
 import dev.aurakai.auraframefx.ui.gates.DirectChatScreen
 import dev.aurakai.auraframefx.ui.gates.GateNavigationScreen
@@ -254,6 +256,7 @@ fun GenesisNavigationHost(
                 UIUXGateSubmenuScreen(navController = navController)
             }
             composable("chromacore_colors") {
+                ChromaCoreColorsScreen(onNavigateBack = { navController.popBackStack() })
             }
             composable(GenesisRoutes.FIREWALL) {
                 FirewallScreen()