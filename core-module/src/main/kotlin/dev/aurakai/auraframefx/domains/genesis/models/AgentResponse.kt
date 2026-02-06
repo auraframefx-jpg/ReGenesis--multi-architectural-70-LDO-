1426dac981bed67361f4bfc0f1488ed322689845
AuraFramefx <wehttam1989@gmail.com>
2026-02-06 16:54:45 -0700

    chore(cleanup): Comment out FakeRomToolsManager and related classes
    This commit comments out the entire `FakeRomToolsManager.kt` file, which includes the `FakeRomToolsManager` itself and several other fake manager implementations used for previews (`FakeBootloaderManager`, `FakeRecoveryManager`, etc.).
    
    Additionally, it introduces a new `AgentResponse` annotation class in the `core-module` to resolve a dependency, replacing the previous concrete class definition.

--- core-module/src/main/kotlin/dev/aurakai/auraframefx/domains/genesis/models/AgentResponse.kt
+++ core-module/src/main/kotlin/dev/aurakai/auraframefx/domains/genesis/models/AgentResponse.kt
@@ -2,3 +2,4 @@ package dev.aurakai.auraframefx.domains.genesis.models
 
 // AgentResponse is defined in the app module
 // This file is intentionally empty to avoid duplicate class definitions
+annotation class AgentResponse
